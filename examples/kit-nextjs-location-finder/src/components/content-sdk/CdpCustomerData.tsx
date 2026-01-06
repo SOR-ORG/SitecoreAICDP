6dda7dbb228a115ccec8e33668e6157c51fea6d7
soreilly6 <sarah.oreilly@sitecore.com>
2026-01-06 16:12:42 +0000

    initial commit for CDP data

---
+++ examples/kit-nextjs-location-finder/src/components/content-sdk/CdpCustomerData.tsx
@@ -0,0 +1,40 @@
+import { RichText as ContentSdkRichText, Field } from '@sitecore-content-sdk/nextjs';
+import { cn } from '@/lib/utils';
+import { NoDataFallback } from '@/utils/NoDataFallback';
+import { ComponentProps } from '@/lib/component-props';
+import { getGuestId } from '@sitecore-cloudsdk/core/browser';
+
+/**
+ * Model used for Sitecore Component integration
+ */
+type RichTextBlockProps = ComponentProps & RichTextFields;
+
+interface RichTextFields {
+  fields: {
+    text: Field<string>;
+  };
+}
+export const Default: React.FC<RichTextBlockProps> = (props) => {
+  const { fields } = props;
+
+  const guestID = getGuestId();
+  const text = props.fields ? (
+    <ContentSdkRichText field={props.fields.text} />
+  ) : (
+    <span className="is-empty-hint">Rich text</span>
+  );
+  const id = props.params.RenderingIdentifier;
+  if (fields) {
+    return (
+      <>
+        <div
+          className={cn('component rich-text', props.params.styles?.trimEnd())}
+          id={id ? id : undefined}
+        >
+          <div className="component-content">GuestID: {guestID}.   {text}</div>
+        </div>
+      </>
+    );
+  }
+  return <NoDataFallback componentName="Rich Text Block" />;
+};
